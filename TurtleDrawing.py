a6d00895ddd77aeef49bb3fa057305baf6f1eecf
AidenGutierrez <113128555+AidenGutierrez@users.noreply.github.com>
2022-09-16 09:29:38 -0700

    Update TurtleDrawing.py

--- TurtleDrawing.py
+++ TurtleDrawing.py
@@ -9,6 +9,7 @@ painter.pensize(7)
 painter.pencolor(color)
 painter.fillcolor(color)
 
+#shell
 painter.penup()
 
 painter.goto(-40,1)
@@ -21,18 +22,72 @@ painter.end_fill()
 painter.penup()
 painter.goto(-120,-120)
 
-color= input("Chose a head color")
-painter.fillcolor(color)
-painter.pencolor(color)
+#head
+headColor= input("Chose a head color")
+painter.fillcolor(headColor)
+painter.pencolor(headColor)
 painter.pendown()
 painter.begin_fill()
 painter.circle(40)
 painter.end_fill()
 
+#L eye and right eye
+painter.pencolor("black")
+painter.penup()
+painter.goto(-150,-70)
+painter.pendown()
+painter.circle(5)
+painter.penup()
+painter.goto(-90,-70)
+painter.pendown()
+painter.circle(5)
+
+#smile
+
+painter.penup()
+painter.goto(-140,-90)
+painter.right(90)
+painter.pendown()
+painter.circle(20,180)
+
+#shell line
+
 painter.penup()
 painter.pencolor("black")
-painter.goto(-80,-80)
+painter.goto(-75,-80)
+painter.left(-90)
 painter.pendown()
-painter.forward(120)
-wn=trtl.screen()
+painter.forward(115)
+
+#L leg
+
+painter.penup()
+painter.goto(-80,-180)
+painter.pencolor(color)
+painter.fillcolor(color)
+
+painter.begin_fill()
+painter.circle(20)
+painter.end_fill()
+
+# R leg
+painter.penup()
+painter.home()
+painter.goto(-10,-180)
+painter.pendown()
+painter.begin_fill()
+painter.circle(20)
+painter.end_fill()
+
+#sand
+painter.penup()
+painter.goto(-300,-180)
+painter.pendown()
+painter.pencolor("brown")
+painter.forward(600)
+
+
+
+wn=trtl.Screen()
+wn.bgcolor("blue")
 wn.mainloop()
