f3f16b3e519e13202dcc74ce03b3da0853f2bbd0
AidenGutierrez <113128555+AidenGutierrez@users.noreply.github.com>
2022-10-06 09:26:53 -0700

    Create challenge7.py

---
+++ challenge7.py
@@ -0,0 +1,9 @@
+number = int(input("Give me a number so I can do some magic with it."))
+
+magic1 = 2 ** number
+magic2 = number * 2
+magic3 = 100 - number
+
+print(" if I do magic number 1 you get,",magic1)
+print(" if I do magic number 2 you get,",magic2)
+print(" if I do magic number 3 you get,",magic3)
